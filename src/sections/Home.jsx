462a347f2270bf555f72d32a73cdd133d0fbe1e1
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-10-23 23:31:01 +0530

    updated with colors

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -3,29 +3,29 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 const Home = () => {
   useGSAP(() => {
-  gsap.to(".cookie-left", {x:500, rotation: 60,y: -50, duration: 10, repeat: -1, yoyo: true, ease: "power1.inOut" });
-  gsap.to(".cookie-right", {x:-500,rotation: 60, y: 50, duration: 10, repeat: -1, yoyo: true, ease: "power1.inOut" });
+  gsap.to(".cookie-left", {x:550, rotation: 60,y: -50, duration: 6, repeat: -1, yoyo: true, ease: "power1.inOut" });
+  gsap.to(".cookie-right", {x:-550,rotation: 60, y: 50, duration: 6, repeat: -1, yoyo: true, ease: "power1.inOut" });
 
-  gsap.to(".drink-left", {scale: 1.1,rotation: 20 , y: -40, duration: 1, repeat: -1, yoyo: true, ease: "power1.inOut" });
-  gsap.to(".drink-right", {scale: 1.1,rotation: -20, y: 40, duration: 1, repeat: -1, yoyo: true, ease: "power1.inOut" });
+  gsap.to(".drink-left", {scale: 1.1,rotation: 20 , y: -40, duration: 2, repeat: -1, yoyo: true, ease: "power1.inOut" });
+  gsap.to(".drink-right", {scale: 1.1,rotation: -20, y: 40, duration: 2, repeat: -1, yoyo: true, ease: "power1.inOut" });
 });
 
 
   return (
     <section>
 
-      <div  className=" min-h-screen flex flex-col justify-center items-center bg-linear-to-r from-white to-yellow-700 text-center px-4  bg-cover bg-center relative ">
+      <div  className=" min-h-screen flex flex-col justify-center items-center bg-linear-to-r from-yellow-100 to-yellow-900 text-center px-4  bg-cover bg-center relative ">
       
         <div className="side-images">
-        <img src="/images/cookie.png"  className="cookie-left absolute top-20 left-5 w-[200px]" alt="" />
-        <img src="/images/cookie.png"  className="cookie-right absolute  bottom-20 right-5 w-[200px]"alt="" />
+        <img src="/images/cookie.png"  className="cookie-left absolute top-20 left-5 w-[150px]" alt="" />
+        <img src="/images/cookie.png"  className="cookie-right absolute  bottom-20 right-5 w-[150px]"alt="" />
 
-        <img src="/images/drinks.png"  className="drink-left absolute  bottom-20 left-10 w-[200px]" alt="" />
-         <img src="/images/drinks.png"  className="drink-right absolute top-20 right-10 w-[200px]" alt="" />
+        <img src="/images/drinks.png"  className="drink-left absolute  bottom-20 left-10 w-[150px]" alt="" />
+         <img src="/images/drinks.png"  className="drink-right absolute top-20 right-10 w-[150px]" alt="" />
         </div>
 
         {/* Shop Name */}
-        <div className="w-150 bg-white/5 backdrop-blur-sm p-10 rounded-3xl shadow-[0_0_40px_rgba(255,255,255,0.3)]">
+        <div className="w-160 bg-white/5 backdrop-blur-sm p-10 rounded-3xl shadow-[0_0_40px_rgba(255,255,255,0.3)] text-shadow">
           <h1 className="text-5xl lg:text-6xl font-bold text-white mb-4">
             Ramdhani Beverages
           </h1>
@@ -41,7 +41,7 @@ const Home = () => {
           </p>
 
           {/* Call to Action */}
-          <button className="bg-yellow-900 hover:bg-yellow-800 text-white px-8 py-3 rounded-lg font-medium shadow-lg transition-all duration-300">
+          <button className="bg-yellow-900 hover:bg-yellow-800 hover:scale-105 shadow-lg text-white px-8 py-3 rounded-lg font-medium transition-all duration-300">
             Learn More...
           </button>
         </div>
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,20 +3,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-5 w-full text-white z-50">
+    <nav className="fixed top-5 w-full text-yellow-100 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-2">
         {/* Logo / Shop Name */}
-        <div className="text-2xl font-bold bg-linear-to-r from-yellow-900 rounded-3xl p-2">
+        <div className="text-2xl font-extrabold bg-linear-to-r from-yellow-900 rounded-3xl p-2 m-2">
           RB
         </div>
 
         {/* Menu Links */}
         <div className="grow flex justify-center">
         <ul className="flex space-x-10">
-          <li><a href="#home" className=" bg-linear-to-r from-yellow-900  p-2 rounded  font-medium hover:text-yellow-400">Home</a></li>
-          <li><a href="#about" className=" bg-linear-to-r from-yellow-900  p-2 rounded font-medium hover:text-yellow-400">About</a></li>
-          <li><a href="#products" className=" bg-linear-to-r from-yellow-900   p-2 rounded font-medium hover:text-yellow-400">Products</a></li>
-          <li><a href="#contact" className=" bg-linear-to-r from-yellow-900  p-2 rounded font-medium hover:text-yellow-400">Contact</a></li>
+          <li><a href="/home" className=" bg-linear-to-r from-yellow-900  p-2 rounded  font-medium hover:text-white">Home</a></li>
+
+          <li><a href="#about" className=" bg-linear-to-r from-yellow-900  p-2 rounded font-medium hover:text-white">About</a></li>
+
+          <li><a href="#products" className=" bg-linear-to-r from-yellow-900   p-2 rounded font-medium hover:text-white">Products</a></li>
+
+          <li><a href="#contact" className=" bg-linear-to-r from-yellow-900  p-2 rounded font-medium hover:text-white">Contact</a></li>
         </ul>
         </div>
       </div>
