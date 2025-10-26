55f59e92d63aa62eb0992e8bd3786feca3ad8ba8
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-10-26 19:03:40 +0530

    Mobile responsive

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -16,31 +16,32 @@ const Home = () => {
       <div  className=" min-h-screen flex flex-col justify-center items-center bg-linear-to-r from-red-100 to-yellow-600 text-center px-4 relative ">
       
         <div className="side-images">
-        <img src="/images/cookie.png"  className="cookie-left absolute top-20 left-5 w-[150px]" alt="cookie" />
-        <img src="/images/cookie.png"  className="cookie-right absolute  bottom-20 right-5 w-[150px]"alt="cookie" />
-
-        <img src="/images/drinks.png"  className="drink-left absolute  bottom-20 left-10 w-[150px]" alt="drinks" />
-         <img src="/images/drinks.png"  className="drink-right absolute top-20 right-10 w-[150px]" alt="drinks" />
-        </div>
+          <img src="/images/cookie.png" className="cookie-left absolute top-10 md:top-20 left-2 md:left-5 w-24 md:w-[150px]" alt="cookie" />
+          <img src="/images/cookie.png" className="cookie-right absolute bottom-10 md:bottom-20 right-2 md:right-5 w-24 md:w-[150px]" alt="cookie" />
+          <img src="/images/drinks.png" className="drink-left absolute bottom-10 md:bottom-20 left-2 md:left-10 w-24 md:w-[150px]" alt="drinks" />
+          <img src="/images/drinks.png" className="drink-right absolute top-10 md:top-20 right-2 md:right-10 w-24 md:w-[150px]" alt="drinks" />
+       </div>
 
         {/* Shop Name */}
-        <div className="w-160 bg-white/5 backdrop-blur-sm p-10 rounded-3xl shadow-[0_0_40px_rgba(255,255,255,0.3)] text-shadow">
-          <h1 className="text-5xl lg:text-6xl font-bold text-white mb-4">
+      <div className="w-full max-w-md md:max-w-2xl lg:max-w-3xl bg-white/5 backdrop-blur-sm p-4 md:p-10 rounded-3xl shadow-[0_0_40px_rgba(255,255,255,0.3)] text-shadow">
+
+
+          <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white mb-4">
             Ramdhani Beverages
           </h1>
 
           {/* Location */}
-          <p className="text-lg lg:text-xl text-white mb-8">
+          <p className="text-base md:text-lg lg:text-xl text-white mb-8">
             Yavatmal, Maharashtra.
           </p>
 
           {/* Tagline */}
-          <p className="text-xl lg:text-2xl font-semibold text-white mb-10">
+          <p className="text-lg md:text-xl lg:text-2xl font-semibold text-white mb-10">
             “Wholesaler of Drinks, Snacks & Sweet Treats - All Under One Roof!”
           </p>
 
           {/* Call to Action */}
-          <button className="bg-yellow-900 hover:bg-yellow-800 hover:scale-105 shadow-lg text-white px-8 py-3 rounded-lg font-medium transition-all duration-300">
+          <button className="bg-yellow-900 hover:bg-yellow-800 hover:scale-105 shadow-lg text-white  px-4 py-2 md:px-8 md:py-3 text-sm md:text-base  rounded-lg font-medium transition-all duration-300">
             Learn More...
           </button>
         </div>
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,25 +1,26 @@
 // Navbar.jsx
-import React from "react";
+
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-5 w-full text-yellow-100 z-50">
-      <div className="max-w-7xl mx-auto flex justify-between items-center p-2">
+    <nav className="fixed top-2 md:top-5 w-full text-yellow-100 z-50">
+      <div className="max-w-7xl mx-auto flex justify-between items-center md:flex-row">
         {/* Logo / Shop Name */}
-        <div className="text-2xl font-extrabold bg-yellow-900 rounded-3xl p-2 m-2">
+        <div className="text-2xl md:text-3xl  font-extrabold bg-yellow-900 rounded-3xl p-2 m-2">
           RB
         </div>
 
         {/* Menu Links */}
         <div className=" flex justify-center">
         <ul className="flex space-x-8">
-          <li><a href="/home" className=" text-yellow-900  p-2 rounded font-medium hover:text-white">Home</a></li>
 
-          <li><a href="/missionsection.jsx" className=" text-yellow-900  p-2 rounded font-medium hover:text-white">About</a></li>
+          <li><a href="/home" className=" text-yellow-900 text-base md:text-lg  p-2 rounded font-medium hover:text-white">Home</a></li>
 
-          <li><a href="#products" className=" text-yellow-900   p-2 rounded font-medium hover:text-white">Products</a></li>
+          <li><a href="/missionsection.jsx" className=" text-yellow-900 text-base md:text-lg  p-2 rounded font-medium hover:text-white">About</a></li>
 
-          <li><a href="#contact" className=" text-yellow-900  p-2 rounded font-medium hover:text-white">Contact</a></li>
+          <li><a href="#products" className=" text-yellow-900  text-base md:text-lg  p-2 rounded font-medium hover:text-white">Products</a></li>
+
+          <li><a href="#contact" className=" text-yellow-900 text-base md:text-lg  p-2 rounded font-medium hover:text-white">Contact</a></li>
         </ul>
         </div>
       </div>
